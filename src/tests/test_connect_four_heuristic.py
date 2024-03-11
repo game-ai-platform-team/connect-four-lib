b6dc60b88d3851f861cd82db13cd6a1bf15bdc38
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2024-03-11 16:58:30 +0200

    fix: Tests

--- src/tests/test_connect_four_heuristic.py
+++ src/tests/test_connect_four_heuristic.py
@@ -140,18 +140,10 @@ class TestConnectFourHeuristic(unittest.TestCase):
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
         ]
-        for i in [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)]:
-        board = [
-            [1, 1, 1, 0, 0, 0],
-            [2, 2, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-        ]
+
         for i in [(0, 0), (0, 1), (0, 2), (1, 0), (1, 1)]:
             heuristic.evaluate_relevant_windows(i[0], i[1], board)
+
         self.assertEqual(heuristic.vertical_windows[0][0], 8)
         self.assertEqual(heuristic.vertical_windows[0][1], 4)
         self.assertEqual(heuristic.vertical_windows[0][2], 2)
@@ -170,16 +162,7 @@ class TestConnectFourHeuristic(unittest.TestCase):
             [0, 0, 0, 0, 0, 0],
             [0, 0, 0, 0, 0, 0],
         ]
-        for i in [(3, 0), (3, 1), (3, 2), (2, 0), (2, 1), (1, 0), (0, 0)]:
-        board = [
-            [2, 0, 0, 0, 0, 0],
-            [1, 0, 0, 0, 0, 0],
-            [2, 1, 0, 0, 0, 0],
-            [1, 2, 1, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-        ]
+
         for i in [(3, 0), (3, 1), (3, 2), (2, 0), (2, 1), (1, 0), (0, 0)]:
             heuristic.evaluate_relevant_windows(i[0], i[1], board)
 
